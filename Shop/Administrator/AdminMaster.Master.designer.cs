662f2c1f763b2758692b9b611853afdd4bf460a9
Koroman91 <korolija91@yahoo.com>
2018-10-31 03:03:23 +0100

    Dvadeseta promena

--- Shop/Administrator/AdminMaster.Master.designer.cs
+++ Shop/Administrator/AdminMaster.Master.designer.cs
@@ -76,6 +76,15 @@ namespace Shop.Administrator {
         protected global::System.Web.UI.WebControls.Image Image2;
         
         /// <summary>
+        /// logoutButton control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button logoutButton;
+        
+        /// <summary>
         /// pnlCategories control.
         /// </summary>
         /// <remarks>
